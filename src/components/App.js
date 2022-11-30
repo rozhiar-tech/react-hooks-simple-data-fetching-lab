d4df9db8eec6f5d73d56f3659dab0aa71748282a
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2022-11-30 10:40:17 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect } from 'react';
+
+function App(){
+    const [dogImage, setDogImage] = React.useState(null);
+
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => setDogImage(data))
+        console.log(dogImage);
+    }, [])
+
+    
+    if(!dogImage) return <p>Loading...</p>
+
+    return(
+        <div>
+        <img src={dogImage.message} alt= "A Random Dog"></img>
+        </div>
+    )
+}
+
+
+export default App;
